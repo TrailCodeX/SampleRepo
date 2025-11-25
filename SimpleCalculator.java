1348849310430559eb063c39191f6d74adba8e70
merinmagi05 <merinmagitelson05@gmail.com>
2025-11-25 16:07:33 +0530

    adding updates

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -6,38 +6,43 @@ public class SimpleCalculator {
         int numOne,numTwo;
         int result;
         char ch;
-        System.out.println(("Enter first number"));
-        numOne=sc.nextInt();
-        System.out.println(("Enter second number"));
-        numTwo=sc.nextInt();
-        sc.nextLine();
-        System.out.println("Enter the operand");
-        ch=sc.next().charAt(0);
-        switch(ch){
-            case '+':
-                result=numOne+numTwo;
-                System.out.println("Result "+result);
-                break;
-            case '-':
-                result=numOne-numTwo;
-                System.out.println("Result "+result);
-                break;
-            case '/':
-                result=numOne/numTwo;
-                System.out.println("Result "+result);
-                break;
-            case '*':
-                result=numOne*numTwo;
-                System.out.println("Result "+result);
-                break;
-            case '%':
-                result=numOne%numTwo;
-                System.out.println("Result "+result);
-                break;
-            default:
-                System.out.println("Enter a valid operand");
-        }
+        int n;
 
+        do {
+            System.out.println(("Enter first number"));
+            numOne=sc.nextInt();
+            System.out.println(("Enter second number"));
+            numTwo=sc.nextInt();
 
+            System.out.println("Choose from the following\n1.Addition\n2. Substraction\n3. Division\n4. Multiplication\n5. Modulus");
+            n = sc.nextInt();
+            switch (n) {
+                case 1:
+                    result = numOne + numTwo;
+                    System.out.println("Result " + result);
+                    break;
+                case 2:
+                    result = numOne - numTwo;
+                    System.out.println("Result " + result);
+                    break;
+                case 3:
+                    System.out.println("Result " + numOne / numTwo);
+                    break;
+                case 4:
+                    result = numOne * numTwo;
+                    System.out.println("Result " + result);
+                    break;
+                case 5:
+                    result = numOne % numTwo;
+                    System.out.println("Result " + result);
+                    break;
+                default:
+                    System.out.println("Enter a valid operator");
+            }
+            System.out.println("Do you want to continue?(Y/N)");
+            ch = sc.next().charAt(0);
+        }while (ch == 'Y'||ch=='y') ;
+
+    System.out.println("Exiting.....");
     }
 }
